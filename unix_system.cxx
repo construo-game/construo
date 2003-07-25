340abc3da8128439360128c038e6d38fed69ac11
Ingo Ruhnke <grumbel@gmail.com>
2003-07-25 13:16:04 +0000

    - fixed missing assert

--- unix_system.cxx
+++ unix_system.cxx
@@ -18,6 +18,7 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <stdio.h>
+#include <assert.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <time.h>
