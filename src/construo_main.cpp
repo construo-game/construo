f3094d7436ab7b289867520b03070e40d6a17947
Ingo Ruhnke <grumbel@gmail.com>
2015-03-24 18:33:28 +0100

    Updated copyright header

--- src/construo_main.cpp
+++ src/construo_main.cpp
@@ -1,18 +1,18 @@
-//  Construo - A wire-frame construction game
-//  Copyright (C) 2002 Ingo Ruhnke <grumbel@gmx.de>
+// Construo - A wire-frame construction game
+// Copyright (C) 2002 Ingo Ruhnke <grumbel@gmail.com>
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <fstream>
 #include "construo.hpp"
@@ -81,7 +81,7 @@ ConstruoMain::on_exit()
             << "  New versions and more information can be found at:\n\n"
             << "    * http://fs.fsf.org/construo/\n\n"
             << "  Comments, critique, suggestions self build\n  construction and patches can be send to:\n\n"
-            << "    * Ingo Ruhnke <grumbel@gmx.de>\n\n" << std::endl;
+            << "    * Ingo Ruhnke <grumbel@gmail.com>\n\n" << std::endl;
 }
 
 void
